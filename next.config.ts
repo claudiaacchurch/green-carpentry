9a9013a70a93ed1622ffc17c0722edef81b3d764
claudiaacchurch <claudiachurch00@gmail.com>
2026-03-07 15:56:45 +0000

    config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/green-carpentry" : "",
+  basePath: "/green-carpentry",
   images: { unoptimized: true },
 };
 
